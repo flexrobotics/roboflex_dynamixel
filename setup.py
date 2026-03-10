768d74ce1dc3a7240bc12b391202cee9f5cf7be3
Colin Prepscius <colinprepscius@gmail.com>
2026-03-10 08:44:12 -0400

    v0.1.23

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class CMakeBuild(build_ext):
             extdir += os.path.sep
 
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+                      '-DPython_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
@@ -50,7 +50,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='roboflex.dynamixel',
-    version='0.1.22',
+    version='0.1.23',
     description='Roboflex Dynamixel Library',
     author='Colin Prepscius',
     author_email='colinprepscius@gmail.com',
@@ -71,7 +71,7 @@ setup(
     keywords = ["dynamixel", "robotics", "middleware", "flexbuffers", "python", "c++", "c++20"],
     license = "MIT",
     python_requires='>=3.6',
-    install_requires=['numpy', 'roboflex'],
+    install_requires=['numpy>=2.0', 'roboflex'],
     ext_modules=[CMakeExtension('roboflex/dynamixel/roboflex_dynamixel_ext')],
     cmdclass=dict(build_ext=CMakeBuild),
     py_modules=['__init__'],
